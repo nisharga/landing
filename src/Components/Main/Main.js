d46271e91cde06759818da7196f7d77a17d71919
Nisharga Kabir <kabirnisharga@gmail.com>
2023-01-08 21:47:30 +0600

    main section update

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -5,35 +5,43 @@ import code3 from "./code3.svg";
 import code4 from "./code4.svg";
 const Main = () => {
   return (
-    <div className="container mx-auto">
-      <div className="single-dev">
-        <h1>Web Development</h1>
-        <img src={code1} alt="code1" />
-        <p>
+    <div className="container mx-auto my-12 grid grid-cols-2 overflow-y-auto text-center md:grid-cols-4">
+      <div className="single-dev p-5">
+        <h1 className="text-lg capitalize text-blue-900 md:text-2xl">
+          Web Development
+        </h1>
+        <img src={code1} alt="code1" className="mx-auto mb-5 h-40 w-40" />
+        <p className="color-gyay-400 text-base md:text-md">
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias
           porro sunt sapiente, est iste dolor.
         </p>
       </div>
-      <div className="single-dev">
-        <h1>Web Design</h1>
-        <img src={code2} alt="code1" />
-        <p>
+      <div className="single-dev p-5">
+        <h1 className="min-h-15 text-lg capitalize text-blue-900 md:text-2xl">
+          Web Design
+        </h1>
+        <img src={code2} alt="code1" className="mx-auto mb-5 h-40 w-40" />
+        <p className="color-gyay-400 text-base md:text-md">
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias
           porro sunt sapiente, est iste dolor.
         </p>
       </div>
-      <div className="single-dev">
-        <h1>Mern Stack</h1>
-        <img src={code3} alt="code1" />
-        <p>
+      <div className="single-dev p-5">
+        <h1 className="min-h-15 text-lg capitalize text-blue-900 md:text-2xl">
+          Mern Stack
+        </h1>
+        <img src={code3} alt="code1" className="mx-auto mb-5 h-40 w-40" />
+        <p className="color-gyay-400 text-base md:text-md">
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias
           porro sunt sapiente, est iste dolor.
         </p>
       </div>
-      <div className="single-dev">
-        <h1>Full Stack</h1>
-        <img src={code4} alt="code1" />
-        <p>
+      <div className="single-dev p-5">
+        <h1 className="min-h-15 text-lg capitalize text-blue-900 md:text-2xl">
+          Full Stack
+        </h1>
+        <img src={code4} alt="code1" className="mx-auto mb-5 h-40 w-40" />
+        <p className="color-gyay-400 text-base md:text-md">
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias
           porro sunt sapiente, est iste dolor.
         </p>
